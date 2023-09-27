7cd3bf642c1857d33135eb485ed5770e1f21913e
Agusco24 <aguscolongne@gmail.com>
2023-09-27 20:43:30 -0300

    Initial commit

---
+++ src/universidadgrupo87/accesoADatos/MateriaData.java
@@ -0,0 +1,153 @@
+package universidadgrupo87.accesoADatos;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import universidadgrupo87.entidades.Materia;
+
+/**
+ *
+ * @author gustavo salgado
+ */
+public class MateriaData {
+
+    private Connection con = null;
+
+    public MateriaData() {
+
+        con = Conexion.getConexion();
+
+    }
+
+    public void guardarMateria(Materia materia) {
+
+        String sql = "INSERT INTO materia(idMateria, nombre , anio , estado)"
+                + "VALUES (? ,? ,? ,?) ";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, materia.getIdMateria());
+            ps.setString(2, materia.getNombre());
+            ps.setInt(3, materia.getAnioMateria());
+            ps.setBoolean(4, materia.isActivo());
+
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                materia.setIdMateria(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Se guardo con exito");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+
+            JOptionPane.showMessageDialog(null, "Error al acceder a la Tabla materia" + ex);
+
+        }
+
+    }
+
+    public Materia buscarMateria(int id) {
+
+        String sql = "SELECT idMateria,nombre,anio,estado FROM materia WHERE idMateria = ? AND estado=1";
+        Materia materia = null;
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                materia = new Materia();
+                materia.setIdMateria(id);
+                materia.setNombre(rs.getString("nombre"));
+                materia.setAnioMateria(rs.getInt("anio"));
+                materia.setActivo(true);
+            } else {
+                JOptionPane.showMessageDialog(null, "no existe la materia");
+
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "error al acceder a la tabla materia");
+        }
+        return materia;
+
+    }
+
+    public void modificarMateria(Materia materia) {
+        String sql = "UPDATE materia SET nombre = ? , anio = ?, estado = ? WHERE idMateria = ? ";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            JOptionPane.showMessageDialog(null,"hola");
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getAnioMateria());
+            ps.setBoolean(3, materia.isActivo());
+            ps.setInt(4, materia.getIdMateria());
+            int exito = ps.executeUpdate();
+              
+            if (exito == 1) {
+
+                JOptionPane.showMessageDialog(null, "materia modificada");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "error al acceder a la tabla materia");
+
+        }
+
+    }
+
+    public void eliminarMateria(int id) {
+        String sql = "UPDATE materia SET estado = 0 WHERE idMateria = ?";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
+
+                JOptionPane.showMessageDialog(null, "materia eliminada con exito");
+            }
+
+        } catch (SQLException ex) {
+
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia");
+
+        }
+    }
+
+    public List<Materia> materias() {
+
+        String sql = "SELECT idMateria,nombre,anio FROM materia WHERE estado=1";
+
+        ArrayList<Materia> materias = new ArrayList<>();
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Materia materia = new Materia();
+                materia.setIdMateria(rs.getInt("idMateria"));
+                materia.setNombre(rs.getString("nombre"));
+                materia.setAnioMateria(rs.getInt("anio"));
+                materia.setActivo(true);
+
+                materias.add(materia);
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "error al acceder a la tabla materia");
+
+        }
+        return materias;
+    }
+}
---
+++ src/universidadgrupo87/entidades/Alumno.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidadgrupo87.entidades;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+/**
+ *
+ * @author Agustin Colongne
+ */
+public class Alumno {
+
+    private int idAlumno;
+    private String nombre;
+    private String apellido;
+    private LocalDate fechaNacimiento;
+    private boolean activo;
+    private int dni;
+
+    public Alumno() {
+    }
+
+    public Alumno(int idAlumno, String nombre, String apellido, LocalDate fechaNacimiento, boolean activo, int dni) {
+        this.idAlumno = idAlumno;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.activo = activo;
+        this.dni = dni;
+    }
+
+    public Alumno(String nombre, String apellido, LocalDate fechaNacimiento, boolean activo, int dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.activo = activo;
+        this.dni = dni;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNac) {
+        this.fechaNacimiento = fechaNac;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellido;
+    }
+
+}
